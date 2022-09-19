ed4ded8f739cd0d7e39cb62494d6aee1a7109b4e
Nitin Kalasannavar <ntnfreelancer@gmail.com>
2022-09-19 22:16:28 +0530

    Implemented Note list page UI (#9)

--- app/javascript/src/components/Dashboard/Notes/constants.js
+++ app/javascript/src/components/Dashboard/Notes/constants.js
@@ -1,5 +1,58 @@
 import * as yup from "yup";
 
+export const NOTES_LIST_DATA = [
+  {
+    id: 1,
+    title: "How to claim the warranty?",
+    description: `Are you getting my texts???" she texted to him. He glanced at it and chuckled under his breath. Of course he was getting them, but if he wasn't getting`,
+    tag: "Getting Started",
+    status: "created",
+    createdAt: "2 hours ago",
+    createdBy: "Oliver Smith",
+    imageUrl: "https://i.pravatar.cc/300",
+  },
+  {
+    id: 2,
+    title: "How to claim the warranty?",
+    description: `Are you getting my texts???" she texted to him. He glanced at it and chuckled under his breath. Of course he was getting them, but if he wasn't getting`,
+    tag: "Onboarding",
+    status: "drafted",
+    createdAt: "2 hours ago",
+    createdBy: "Oliver Smith",
+    imageUrl: "https://i.pravatar.cc/300",
+  },
+  {
+    id: 3,
+    title: "How to claim the warranty?",
+    description: `Are you getting my texts???" she texted to him. He glanced at it and chuckled under his breath. Of course he was getting them, but if he wasn't getting`,
+    tag: "User Flow",
+    status: "drafted",
+    createdAt: "2 hours ago",
+    createdBy: "Jon Paul",
+    imageUrl: "",
+  },
+  {
+    id: 4,
+    title: "How to claim the warranty?",
+    description: `Are you getting my texts???" she texted to him. He glanced at it and chuckled under his breath. Of course he was getting them, but if he wasn't getting`,
+    tag: "UX",
+    status: "drafted",
+    createdAt: "2 hours ago",
+    createdBy: "Oliver Smith",
+    imageUrl: "https://i.pravatar.cc/300",
+  },
+  {
+    id: 5,
+    title: "How to claim the warranty?",
+    description: `Are you getting my texts???" she texted to him. He glanced at it and chuckled under his breath. Of course he was getting them, but if he wasn't getting`,
+    tag: "Bug",
+    status: "drafted",
+    createdAt: "2 hours ago",
+    createdBy: "Oliver Smith",
+    imageUrl: "https://i.pravatar.cc/300",
+  },
+];
+
 export const NOTES_FORM_INITIAL_FORM_VALUES = {
   title: "",
   description: "",
